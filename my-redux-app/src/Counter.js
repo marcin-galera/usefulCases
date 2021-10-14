8884e19ac4c3311283a1272f36b864282d37ea15
Marcin Galera <marcin.galera@outlook.com>
2021-10-14 10:40:32 +0200

    done redux

---
+++ my-redux-app/src/Counter.js
@@ -0,0 +1,22 @@
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, set } from "./actions";
+
+export const Counter = () => {
+  const incident = "Incident";
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  return (
+    <main className="Counter">
+      <h1>Days Since Last {incident}</h1>
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(set(0))}>Reset</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </section>
+    </main>
+  );
+};
+
+export default Counter;
